9a5ab8ef9a1ed4e74365de5f77342cc97f67b499
gk007kannan <35525377+gk007kannan@users.noreply.github.com>
2018-02-12 16:52:50 +0530

    Create eryrt.c

---
+++ eryrt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char [20];
+ int i = 0, l = 0, f = 1;
+ clrscr();
+   puts("Enter any string\n");
+   gets(str);
+   for(i = 0; str[i] !='\0'; i++)
+   {
+      l = l + 1;
+   }
+   printf("The number of characters in the string are %d\n", l);
+   for(i = 0; i <= l-1; i++)
+   {
+      if(str[i] == ' ')
+      {
+    f = f + 1;
+      }
+   }
+    return 0;
+}
